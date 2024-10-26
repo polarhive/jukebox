c21a028fbf4b4b0e123e82dd16a4bec3c5a6d6cd
Nathan Paul <mail@polarhive.net>
2024-10-26 20:43:45 +0530

    logs: dump to /tmp

--- src/jukebox_fm/jukebox.py
+++ src/jukebox_fm/jukebox.py
@@ -2,41 +2,46 @@ from os import path, makedirs
 from sys import exit
 from time import sleep
 from argparse import ArgumentParser
-from logging import basicConfig, info, error, warning
+from logging import basicConfig, info, error, warning, StreamHandler, Formatter, getLogger
 from toml import load, dump
 from mpd import MPDClient
 from yt_dlp import YoutubeDL
 from requests import get, RequestException
+client = MPDClient() # setup mpd(1) ref: https://github.com/polarhive/dotfiles/
 
-client = MPDClient()
-
-def main():
+def main():    
     args = parse_arguments()
-    basicConfig(level=args.l, format='%(asctime)s - %(levelname)s - %(message)s')
+    basicConfig(level=args.l, format='%(asctime)s - %(levelname)s - %(message)s', filename='/tmp/jukebox-fm.log', filemode='a')
+    console_handler = StreamHandler(); console_handler.setLevel(args.l); console_handler.setFormatter(Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    getLogger().addHandler(console_handler)
+
+    # toml
     config = load_config(path.expanduser("~/.config/jukebox-fm/config.toml"))
     music_folder = path.expanduser(config['music']['music_folder'])
-    username = args.u or config['lastfm']['username']
-    mode = args.m or config['lastfm']['mode']
+    username = args.u if args.u else config['lastfm']['username']
+    mode = args.m if args.m else config['lastfm']['mode']
+    friends = args.f # friend mode enabled
     endpoint = determine_endpoint(args, username, mode)
     
+    # check if we can connect to the local MPD server
     try:
         client.connect('localhost', 6600)
         ydl_config = configure_ydl(config, music_folder)
         
         if args.f:
-            friends = load_friends(args.f)
+            if friends == True: # -f but no path override 
+                friends = path.expanduser(config['friends']['friends_file'])
+            friends = load_friends(friends)
             mode = args.m or "library"
             for username in friends:
+                info(f"fetching: {username}")
                 fetch_lastfm_data(f"user/{username}/{mode}", music_folder, ydl_config)
-        else: 
+        else:
             fetch_lastfm_data(endpoint, music_folder, ydl_config)
 
-    except KeyboardInterrupt:
-        info("Interrupted by user")
-    except Exception as e:
-        log_error(e)
-    finally:
-        client.disconnect()
+    except KeyboardInterrupt: info("Interrupted by user")
+    except Exception as e: log_error(e)
+    finally: client.disconnect()
 
 def parse_arguments():
     parser = ArgumentParser(description="Play LastFM music recommendations using: mpd, yt-dlp and python")
@@ -45,25 +50,19 @@ def parse_arguments():
     parser.add_argument("-p", help="Play a specified playlist")
     parser.add_argument("-u", help="Custom LastFM username", default=None)
     parser.add_argument("-m", help="Custom LastFM mode", default=None)
-    parser.add_argument("-f", help="Path to LastFM usernames friends.txt", default=None)
+    parser.add_argument("-f", help="Path to LastFM usernames friends.txt", nargs='?', const=True, default=None)
     parser.add_argument("-l", help="Set logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)", default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
     return parser.parse_args()
 
 def load_config(config_path):
-    if not path.exists(config_path):
-        create_default_config(config_path)
-
-    try:
-        return load(config_path)
-    except Exception as e:
-        log_error(f"Could not load configuration file: {e}")
+    if not path.exists(config_path): create_default_config(config_path)
+    try: return load(config_path)
+    except Exception as e: log_error(f"Could not load configuration file: {e}")
 
 def create_default_config(config_path):
     config_dir = path.dirname(config_path)
-    try:
-        makedirs(config_dir, exist_ok=True)
-    except Exception as e:
-        log_error(f"Could not create configuration directory: {e}")
+    try: makedirs(config_dir, exist_ok=True)
+    except Exception as e: log_error(f"Could not create configuration directory: {e}")
 
     default_config = {
         'music': {'music_folder': '~/Music/dl'},
@@ -75,20 +74,20 @@ def create_default_config(config_path):
             'geo_bypass': True,
             'outtmpl': '%(artist)s - %(title)s',
             'preferred_quality': '192'
-        }
+        },
+        'friends': {'friends_file': '~/config/jukebox-fm/friends.txt'}
     }
     
     try:
-        with open(config_path, 'w') as config_file:
-            dump(default_config, config_file)
+        with open(config_path, 'w') as config_file: dump(default_config, config_file)
         info(f"Created default configuration file at {config_path}")
-    except Exception as e:
-        log_error(f"Could not create default configuration file: {e}")
+    except Exception as e: log_error(f"Could not create default configuration file: {e}")
 
+# endpoint URL
 def determine_endpoint(args, username, mode):
     if args.a: return f"music/{args.a}"
     elif args.g: return f"tag/{args.g}"
-    elif args.p: return f"user/{username}/playlist/{args.p}"
+    elif args.p: return args.p.replace("https://www.last.fm/", "").replace("user/", "user/")
     else: return f"user/{username}/{mode}"
     
 def configure_ydl(config, music_folder):
@@ -108,32 +107,27 @@ def configure_ydl(config, music_folder):
 def load_friends(friends_file):
     try:
         with open(friends_file, 'r') as file:
-            return [line.strip() for line in file if line.strip()]
-    except Exception as e:
-        log_error(f"Could not read friends file: {e}")
+            lines = [line.strip() for line in file if line.strip()]
+            if not lines: log_error("friends.txt empty.")
+            else: return lines
+    except Exception as e: log_error(f"Could not read friends.txt: {e}")
 
 def fetch_lastfm_data(endpoint, music_folder, ydl_config):
     url = f"https://www.last.fm/player/station/{endpoint}"
     try:
-        response = get(url)
-        response.raise_for_status()
+        response = get(url); response.raise_for_status()
         playlist = response.json().get("playlist", [])
 
-        if not playlist:
-            info("No tracks found in the playlist.")
-            return
-
+        if not playlist: info("No tracks found in the playlist."); return
+        
         parsed_tracks = parse_tracks(playlist)
-
+        
         info(f"Fetched: {len(parsed_tracks)} tracks")
         for artist, title, playlink_id in parsed_tracks:
-            if is_track_in_library(artist, title):
-                queue_song(f"{artist} - {title}")
-            else:
-                download_song(artist, title, playlink_id, music_folder, ydl_config)
-
-    except RequestException as e:
-        log_error(f"Can't fetch data from LastFM: {e}")
+            if is_track_in_library(artist, title): queue_song(f"{artist} - {title}")
+            else: download_song(artist, title, playlink_id, music_folder, ydl_config)
+            
+    except RequestException as e: log_error(f"Can't fetch data from LastFM: {e}")
 
 def parse_tracks(playlist):
     parsed_tracks = []
@@ -142,12 +136,9 @@ def parse_tracks(playlist):
             artist = track['artists'][0].get("name")
             title = track.get("name")
             playlink_id = track['_playlinks'][0].get("id")
-            if artist and title and playlink_id:
-                parsed_tracks.append((artist, title, playlink_id))
-            else:
-                warning(f"Incomplete track data: {track}")
-        except (IndexError, KeyError) as e:
-            warning(f"Error parsing track data: {e}")
+            if artist and title and playlink_id: parsed_tracks.append((artist, title, playlink_id))
+            else: warning(f"Incomplete track data: {track}")
+        except (IndexError, KeyError) as e: warning(f"Error parsing track data: {e}")
 
     return parsed_tracks
 
@@ -167,7 +158,7 @@ def queue_song(song):
         info(f"Queued: {song}")
     except Exception as e:
         log_error(f"Could not queue song '{song}': {e}")
-
+        
 def download_song(artist, title, playlink_id, music_folder, ydl_config):
     youtube_url = f"https://www.youtube.com/watch?v={playlink_id}"
     ydl_opts = {
@@ -184,9 +175,8 @@ def download_song(artist, title, playlink_id, music_folder, ydl_config):
         try:
             ydl.download([youtube_url])
             queue_song(song)
-            info(f"Downloaded and queued: {song}")
         except Exception as e:
-            log_error(f"Could not download song '{song}': {e}")
+            error(f"Could not download song '{song}': {e}")
 
 def log_error(message):
     error(message)
